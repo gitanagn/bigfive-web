f20f8b3a13273645817cd795b1ab9d1a4ba74b8c
Gitana Ginkevice <gitanagn@users.noreply.github.com>
2025-07-18 11:18:56 +0300

    Update middleware.ts

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -11,8 +11,8 @@ export default createMiddleware({
 export const config = {
   // Match only internationalized pathnames
   matcher: [
-    '/',
-    '/(en|ar|de|es|fr|id|it|no|pt|sv|uk|da|fi|hi|is|ja|pl|ru|th|zh)/:path*',
-    '/((?!_next|_vercel|.*\\..*).*)'
-  ]
+  '/',
+  '/(en|lt|ar|de|es|fr|id|it|no|pt|sv|uk|da|fi|hi|is|ja|pl|ru|th|zh)/:path*',
+  '/((?!_next|_vercel|.*\\..*).*)'
+]
 };
