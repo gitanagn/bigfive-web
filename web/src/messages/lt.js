a726812d99d4aec2c977fbd2cb473a1bc700e625
Gitana Ginkevice <gitanagn@users.noreply.github.com>
2025-07-18 13:27:18 +0300

    Update full lt.js

--- web/src/messages/lt.js
+++ web/src/messages/lt.js
@@ -1,54 +1,220 @@
 const lithuanian = {
-  "language": {
-    "label": "Lietuvių",
-    "locale": "lt"
+  language: {
+    label: 'Lithuanian',
+    locale: 'lt'
   },
-  "seo": {
-    "keywords": "didžiojo penketo asmenybės testas, didysis penketas, b5 testas, asmenybės bruožai, lyginimas, nemokamas, jordan peterson"
+  seo: {
+    keywords:
+      'didžiojo penketo asmenybės bruožų testas, didysis penketas, b5 testas, bigfive testas, asmenybės bruožai, bigfive, palyginti, nemokamas, jordan peterson'
   },
-  "frontpage": {
-    "seo": {
-      "title": "Nemokamas atviro kodo Didžiojo Penketo asmenybės testas",
-      "description": "Atlik nemokamą, atviro kodo Didžiojo Penketo asmenybės testą. Sužinok savo asmenybės bruožus ir palygink save su partneriu, kolegomis, draugais ar šeima."
+  frontpage: {
+    seo: {
+      title: 'Didžiojo penketo asmenybės bruožų testas',
+      description:
+        'Atlikite didžiojo penketo asmenybės bruožų testą ir sužinokite dominuojančius savo asmenybės bruožus. Palyginkite juos su partnerio, kolegų, draugų rezultatais.'
     },
-    "title": "Didžiojo Penketo Asmenybės Testas",
-    "call_to_action": "Pradėk testą dabar",
-    "no_registration": "* Registracija nereikalinga",
-    "tests_taken": "Daugiau nei <green>{n}</green> žmonių atliko testą",
-    "compare": {
-      "title": "Palygink save su kitais",
-      "text1": "Palygink save su partneriu, kolegomis, draugais ar šeima.",
-      "text2": "Sužinok, kaip skiriesi nuo kitų žmonių penkiose srityse:",
-      "action": "Pamatyk tai gyvai"
+    title: 'Didžiojo penketo asmenybės bruožų testas',
+    call_to_action: 'Pradėti testą dabar',
+    no_registration: '* Jokių registracijų',
+    // do not translate <green>, {n}, and </green> tags
+    tests_taken: 'Virš <green>{n}</green> žmonių atliko testą',
+    compare: {
+      title: 'Palyginti save su kitais',
+      text1:
+        'Palyginkite save su partneriu, kolega ar draugais.',
+      text2: 'Sužinokite, kuo skiriatės nuo kitų žmonių penkiose asmenybės srityse:',
+      action: 'Pamatyti gyvai'
     },
-    "share": "Pasidalink su draugais ir sužinokite, kaip sutampate",
-    "cards": {
-      "open": {
-        "title": "Atvira",
-        "text": "Tai atviro kodo projektas pagal MIT licenciją."
+    share: 'Pasidalykite su draugais ir sužinokite, kiek esate suderinami',
+    cards: {
+      open: {
+        title: 'Tikrumas',
+        text: 'Rezultatai yra nekintantys. Atskleiskite savo savybes.'
       },
-      "free": {
-        "title": "Nemokama",
-        "text": "Testas yra visiškai nemokamas"
+      free: {
+        title: 'Nemokama',
+        text: 'Testas yra nemokamas. Atsakykite į klausimas ir pažinkite save.'
       },
-      "scientific": {
-        "title": "Mokslinis",
-        "text": "Didysis Penketas yra moksliškai patvirtintas ir patikimas psichologinis modelis."
+      scientific: {
+        title: 'Grįsta mokslu',
+        text: 'Moksliškai patvirtintas ir patikimas psichologinis testas.'
       },
-      "translated": {
-        "title": "Išverstas",
-        "text": "Išverstas į daugiau nei 20 kalbų. Padėkite <a href=\"https://b5.translations.alheimsins.net/\" rel=\"noreferrer\" target=\"blank\">šiame vertimų puslapyje</a>!"
+      translated: {
+        title: 'Pasaulinis pripažinimas',
+        text: 'Išversta į daugiau nei 20 kalbų. Naudojamas visame pasaulyje'
       }
     },
-    "description": {
-      "top": "Sužinok save geriau naudodamasis <violet>nemokamu</violet> atviro kodo asmenybės testu.",
-      "subtop": "Didysis Penketas",
-      "subtop2": " yra moksliškai patvirtintas ir patikimiausias psichologinis modelis asmenybei vertinti.",
-      "reference": "Testai ir įvertinimas šioje svetainėje remiasi <a href=\"http://ipip.ori.org\" rel=\"noopener\" target=\"_blank\">ipip.ori.org</a>, klausimynas iš <i>Johnson (2014) 120-item IPIP NEO-PI-R</i>.",
-      "info": "Šis testas susideda iš 120 klausimų ir jo atlikimas trunka apie 10 minučių.",
-      "result": "Baigę testą, gausite išsamų asmenybės rezultatų aprašymą šiose srityse:",
-      "tests_taken": "jau atlikta testų"
+    description: {
+      top: 'Pažink save geriau, atlikęs <violet>mokslu grįstą</violet> psichologijos testą.',
+      subtop: 'Tai',
+      subtop2:
+        ' pasaulyje labiausiai pripažintas psichologinis testas, kurio veiksmingumas įrodytas mokslu.',
+      reference: `Testas ir jo rezultatų analizė paremta
+<a href="http://ipip.ori.org" rel="noopener" target="_blank">ipip.ori.org</a>,
+inventoriumi iš <i>Johnson's (2014) 120-item IPIP NEO-PI-R</i>.`,
+      info: 'Testą sudaro 120 klausimų, testui atlikti skirta 10 minučių.',
+      result:
+        'Atlikę testą, gausite išsamų savo asmenybės bruožų aprašymą šiose srityse:',
+      tests_taken: 'iki šiol atlikta testų'
     }
+  },
+  about: {
+    seo: {
+      title: 'Apie komandą',
+      description:
+        '„Didysis penketas“ turi labai aktyvią ir įsitraukusią komandą, kuri nuolat siekia tobulinti projektą.'
+    }
+  },
+  toolbar: {
+    home: 'Pradžia',
+    result: 'Rezultatai',
+    compare: 'Palyginimas',
+    articles: 'Straipsniai',
+    privacy: 'Privatumas',
+    about: 'Apie',
+    see_results: 'Žiūrėti rezultatus',
+    compare_with: 'Palyginti su kitais',
+    faq: 'DUK'
+  },
+  facets: {
+    openness_to_experience: {
+      title: 'Atvirumas patirčiai'
+    },
+    conscientiousness: {
+      title: 'Sąmoningumas'
+    },
+    extraversion: {
+      title: 'Ekstravertiškumas'
+    },
+    agreeableness: {
+      title: 'Sutarumas'
+    },
+    neuroticism: {
+      title: 'Neurotiškumas'
+    }
+  },
+  common: {
+    and: 'ir',
+    pages: 'puslapiai',
+    languages: 'kalbos',
+    save: 'Saugoti',
+    close: 'Uždaryti'
+  },
+  form: {
+    information: 'Informacija',
+    informationText: 'Mes naudojame jūsų pateiktą informaciją tam, kad galėtume pateikti jums',
+    mostAccurate: 'tiksliausią įmanomą rezultatą',
+    readMoreAbout: 'Skaityti išsamiau',
+    privacy: 'privatumas',
+    nextButton: 'Gerai, kitas klausimas',
+    declineButton: 'tęsti be dalinimosi',
+    prefferedLanguage: 'Mano kalba yra',
+    selectLanguage: 'Pasirinkti kalbą',
+    iama: 'Aš esu',
+    male: 'Vyras',
+    female: 'Moteris',
+    age: 'Amžius',
+    iam: 'Aš esu',
+    yearsOld: 'metų',
+    ageWarning: 'Privalote būti 16 ar vyresnis',
+    language: 'Kalba',
+    gender: 'Lytis',
+    confirmInfo: 'Patvirtinti informaciją',
+    confirm: 'Patvirtinti',
+    toTestButton: 'Gerai, laikyti testą'
+  },
+  test: {
+    next: 'kitas',
+    back: 'atgal',
+    more: 'daugiau',
+    seeResults: 'žiūrėti rezultatus'
+  },
+  big_five: {
+    title: 'Kas yra Didydis penketas?',
+    seo: {
+      title: 'Kas yra Didydis penketas?',
+      description: 'Skaityti išsamiau apie didyjį penketą'
+    }
+  },
+  openness_to_experience: {
+    title: 'Atvirumas patirčiai',
+    seo: {
+      title: 'Apžvalga apie bruožą - atvirumas patirčiai.',
+      description:
+        'Skaityti išsamiau apie atvirumo patirčiai bruožą'
+    }
+  },
+  conscientiousness: {
+    title: 'Sąmoningumas',
+    seo: {
+      title: 'Apžvalga apie bruožą - sąmoningumas.',
+      description:
+        'Skaityti išsamiau apie sąmoningumo bruožą'
+    }
+  },
+  extraversion: {
+    title: 'Ektravertiškumas',
+    seo: {
+      title: 'Apžvalga apie bruožą - ekstravertiškumas',
+      description: 'Skaityti išsamiau apie ekstravertiškumo bruožą'
+    }
+  },
+  agreeableness: {
+    title: 'Sutarumas',
+    seo: {
+      title: 'Apžvalga apie bruožą - sutarumas',
+      description: 'Skaityti išsamiau apie sutarumo bruožą'
+    }
+  },
+  neuroticism: {
+    title: 'Neurotiškumas',
+    seo: {
+      title: 'Apžvalga apie bruožą - neurotiškumas',
+      description: 'Skaityti išsamiau apie neurotiškumo bruožą'
+    }
+  },
+  getCompare: {
+    title: 'Palyginti žmones ar komandas',
+    description1:
+      'Palyginkite didžiojo penketo asmenybės testo rezultatus su kelių žmonių rezultatais.',
+    description2: 'Įveskite ID, kurį gavote po testo atlikimo, pvz. ',
+    description3: 'į ID įvedimo laukelį',
+    needToAddPeople: 'Turite pridėti žmonių, kad galėtumėte juos palyginti',
+    addAnother: 'Pridėti kitą žmogų',
+    addPerson: 'Pridėti žmogų',
+    comparePeople: 'Palyginti žmones',
+    nameOfPerson: 'Žmogaus vardas',
+    urlOrId: 'URL arba ID palyginimui',
+    name: 'Vardas'
+  },
+  results: {
+    theBigFive: 'Didysis penketas',
+    score: 'balai',
+    important: 'Svarbu!',
+    saveResults: 'Išsaugokite šį ID, kad galėtumėte vėliau peržiūrėti rezultatus arba ',
+    compare: 'palyginti',
+    toOthers: 'save su kitais',
+    seo: {
+      title: 'Mano Didžiojo penketo asmenybės bruožų testas',
+      description:
+        'Peržiūrėkite mano asmenybės bruožus ir palyginkite save šiose penkiose srityse: neurotiškumas, atvirumas patirčiai, sąmoningumas, ekstraversija, sutariamumas.'
+    }
+  },
+  getResult: {
+    result: 'Rezultatai',
+    explanation:
+      'Jei jau atlikote testą ir išsaugojote savo ID, rezultatus galite peržiūrėti įvesdami gautą ID, t. y. ',
+    idInput: 'į ID įvedimo laukelį',
+    urlOrId: 'jūsų rezultatų URL arba ID',
+    getResult: 'Peržiūrėti rezultatus',
+    viewPrevious: 'Žiūrėti ankstesnius rezultatus'
+  },
+  shareLinks: {
+    copyLink: 'Kopijuoti į iškarpinę',
+    shareFacebook: 'Pasidalinti Facebook',
+    shareTwitter: 'Pasidalinti X',
+    copiedLink: 'Kopijuoti nuorodą',
+    shareResults: 'Pasidalinti rezultatais!'
   }
 };
 
