faa26a0a35e1b1073d78a2321fe6c72ddda23a7f
Gitana Ginkevice <gitanagn@users.noreply.github.com>
2025-07-18 11:03:51 +0300

    Create lt.js

---
+++ web/src/messages/lt.js
@@ -0,0 +1,55 @@
+const lithuanian = {
+  "language": {
+    "label": "Lietuvių",
+    "locale": "lt"
+  },
+  "seo": {
+    "keywords": "didžiojo penketo asmenybės testas, didysis penketas, b5 testas, asmenybės bruožai, lyginimas, nemokamas, jordan peterson"
+  },
+  "frontpage": {
+    "seo": {
+      "title": "Nemokamas atviro kodo Didžiojo Penketo asmenybės testas",
+      "description": "Atlik nemokamą, atviro kodo Didžiojo Penketo asmenybės testą. Sužinok savo asmenybės bruožus ir palygink save su partneriu, kolegomis, draugais ar šeima."
+    },
+    "title": "Didžiojo Penketo Asmenybės Testas",
+    "call_to_action": "Pradėk testą dabar",
+    "no_registration": "* Registracija nereikalinga",
+    "tests_taken": "Daugiau nei <green>{n}</green> žmonių atliko testą",
+    "compare": {
+      "title": "Palygink save su kitais",
+      "text1": "Palygink save su partneriu, kolegomis, draugais ar šeima.",
+      "text2": "Sužinok, kaip skiriesi nuo kitų žmonių penkiose srityse:",
+      "action": "Pamatyk tai gyvai"
+    },
+    "share": "Pasidalink su draugais ir sužinokite, kaip sutampate",
+    "cards": {
+      "open": {
+        "title": "Atvira",
+        "text": "Tai atviro kodo projektas pagal MIT licenciją."
+      },
+      "free": {
+        "title": "Nemokama",
+        "text": "Testas yra visiškai nemokamas"
+      },
+      "scientific": {
+        "title": "Mokslinis",
+        "text": "Didysis Penketas yra moksliškai patvirtintas ir patikimas psichologinis modelis."
+      },
+      "translated": {
+        "title": "Išverstas",
+        "text": "Išverstas į daugiau nei 20 kalbų. Padėkite <a href=\"https://b5.translations.alheimsins.net/\" rel=\"noreferrer\" target=\"blank\">šiame vertimų puslapyje</a>!"
+      }
+    },
+    "description": {
+      "top": "Sužinok save geriau naudodamasis <violet>nemokamu</violet> atviro kodo asmenybės testu.",
+      "subtop": "Didysis Penketas",
+      "subtop2": " yra moksliškai patvirtintas ir patikimiausias psichologinis modelis asmenybei vertinti.",
+      "reference": "Testai ir įvertinimas šioje svetainėje remiasi <a href=\"http://ipip.ori.org\" rel=\"noopener\" target=\"_blank\">ipip.ori.org</a>, klausimynas iš <i>Johnson (2014) 120-item IPIP NEO-PI-R</i>.",
+      "info": "Šis testas susideda iš 120 klausimų ir jo atlikimas trunka apie 10 minučių.",
+      "result": "Baigę testą, gausite išsamų asmenybės rezultatų aprašymą šiose srityse:",
+      "tests_taken": "jau atlikta testų"
+    }
+  }
+};
+
+export default lithuanian;
