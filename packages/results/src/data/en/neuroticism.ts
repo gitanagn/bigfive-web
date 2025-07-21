efefc501a5cb5bb894ed3c2812ac600798d06fdf
Gitana Ginkevice <gitanagn@users.noreply.github.com>
2025-07-21 16:06:05 +0300

    Update neuroticism.ts

--- packages/results/src/data/en/neuroticism.ts
+++ packages/results/src/data/en/neuroticism.ts
@@ -2,106 +2,69 @@ import { type TemplateDomain } from '../../types'
 
 const neuroticism: TemplateDomain = {
   domain: 'N',
-  title: 'Neuroticism',
+  title: 'Neurotiškumas',
   shortDescription: 'Neuroticism refers to the tendency to experience negative feelings.',
-  description: `Freud originally used the term neurosis to describe a
-condition marked by mental distress, emotional suffering, and an
-inability to cope effectively with the normal demands of life. He
-suggested that everyone shows some signs of neurosis, but that we
-differ in our degree of suffering and our specific symptoms of
-distress. Today neuroticism refers to the tendency to experience
-negative feelings. <br /><br />Those who score high on Neuroticism may
-experience primarily one specific negative feeling such as anxiety,
-anger, or depression, but are likely to experience several of these
-emotions. <br /><br />People high in neuroticism are emotionally reactive. They
-respond emotionally to events that would not affect most people, and
-their reactions tend to be more intense than normal. They are more
-likely to interpret ordinary situations as threatening, and minor
-frustrations as hopelessly difficult. <br /><br />Their negative emotional
-reactions tend to persist for unusually long periods of time, which
-means they are often in a bad mood. These problems in emotional
-regulation can diminish a neurotic's ability to think clearly, make
-decisions, and cope effectively with stress.`,
+  description: `Neurotiškumas reiškia polinkį patirti neigiamus jausmus.',
+aprašymas: „Freudas iš pradžių vartojo terminą neurozė apibūdindamas
+būseną, kuriai būdingas psichinis stresas, emocinės kančios ir
+nesugebėjimas efektyviai susidoroti su įprastais gyvenimo reikalavimais. Jis
+teigė, kad kiekvienas žmogus turi tam tikrų neurozės požymių, tačiau
+mes skiriamės savo kančios laipsniu ir specifiniais streso simptomais. Šiandien neurotiškumas reiškia polinkį patirti
+neigiamus jausmus.“. <br /><br />Tie, kurie turi aukštą neurotiškumo balą, gali patirti daugiausia vieną konkretų neigiamą jausmą, pavyzdžiui, nerimą, pyktį ar depresiją, tačiau greičiausiai patirs ir kelias iš šių emocijų. <br /><br />Žmonės, turintys didelį neurotiškumą, yra emociškai reaktyvūs. Jie emociškai reaguoja į įvykius, kurie neturėtų įtakos daugumai žmonių, ir
+jų reakcijos paprastai būna intensyvesnės nei įprastai. Jie labiau linkę įprastas situacijas interpretuoti kaip grėsmingas, o nedidelius
+nusiskumus – kaip beviltiškai sunkius. <br /><br />Jų neigiamos emocinės reakcijos paprastai išlieka neįprastai ilgai, o tai reiškia, kad jie dažnai būna blogos nuotaikos. Šios emocijų reguliavimo problemos gali sumažinti neurotiko gebėjimą aiškiai mąstyti, priimti sprendimus ir veiksmingai susidoroti su stresu.`,
   results: [
     {
       score: 'low', // do not translate this line
-      text: `Your score on Neuroticism is low, indicating that you are
-exceptionally calm, composed and unflappable. You do not react with
-intense emotions, even to situations that most people would describe
-as stressful.`
+      text: `Jūsų neurotiškumo balas yra žemas, o tai rodo, kad esate
+išskirtinai ramus, susikaupęs ir nesutrikdomas. Jūs nereaguojate
+smarkiai emociškai, net į situacijas, kurias dauguma žmonių apibūdintų
+kaip stresines.`
     },
     {
       score: 'neutral', // do not translate this line
-      text: `Your score on Neuroticism is average, indicating that your level of
-emotional reactivity is typical of the general population.
-Stressful and frustrating situations are somewhat upsetting to you,
-but you are generally able to get over these feelings and cope with
-these situations.`
+      text: `Jūsų neurotiškumo balas yra vidutinis, o tai rodo, kad jūsų emocinio reaktyvumo lygis yra tipiškas bendrajai populiacijai.
+Stresinės ir frustruojančios situacijos jus šiek tiek slegia,
+tačiau paprastai galite įveikti šiuos jausmus ir susidoroti su
+šiomis situacijomis.`
     },
     {
       score: 'high', // do not translate this line
-      text: `Your score on Neuroticism is high, indicating that you are easily
-upset, even by what most people consider the normal demands of
-living. People consider you to be sensitive and emotional.`
+      text: `Jūsų neurotiškumo balas yra aukštas, o tai rodo, kad jus lengvai suerzina net ir tai, ką dauguma žmonių laiko įprastais gyvenimo reikalavimais. Žmonės jus laiko jautriu ir emocingu.`
     }
   ],
   facets: [
     {
       facet: 1,
-      title: 'Anxiety',
-      text: `The "fight-or-flight" system of the brain of anxious
-individuals is too easily and too often engaged. Therefore, people who
-are high in anxiety often feel like something dangerous is about to happen.
-They may be afraid of specific situations or be just generally fearful.
-They feel tense, jittery, and nervous. Persons low in Anxiety are generally
-calm and fearless.`
+      title: 'Nerimas',
+      text: `Nerimastingų asmenų smegenų „kovok arba bėk“ sistema yra pernelyg lengvai ir per dažnai įsitraukianti. Todėl žmonės, kurie yra labai nerimastingi, dažnai jaučia, kad tuoj nutiks kažkas pavojingo. Jie gali bijoti konkrečių situacijų arba tiesiog būti išsigandę. Jie jaučiasi įsitempę, nervingi ir nervingi. Žmonės, kurių nerimas silpnas, paprastai yra ramūs ir bebaimiai.`
     },
     {
       facet: 2,
-      title: 'Anger',
-      text: `Persons who score high in Anger feel enraged when
-things do not go their way. They are sensitive about being treated fairly
-and feel resentful and bitter when they feel they are being cheated.
-This scale measures the tendency to feel angry; whether or not the
-person expresses annoyance and hostility depends on the individual's
-level on Agreeableness. Low scorers do not get angry often or easily.`
+      title: 'Pyktis',
+      text: `Žmonės, kurių pykčio balas aukštas, jaučiasi įniršę, kai viskas klostosi ne taip, kaip jiems patinka. Jie jautrūs tam, kad su jais būtų elgiamasi sąžiningai, ir jaučia apmaudą bei kartėlį, kai jaučiasi apgaudinėjami. Ši skalė matuoja polinkį pykti; tai, ar žmogus išreiškia susierzinimą ir priešiškumą, priklauso nuo asmens sutarimo lygio. Žemas balas surinkę asmenys dažnai ir lengvai nesupyksta.`
     },
     {
       facet: 3,
-      title: 'Depression',
-      text: `This scale measures the tendency to feel sad, dejected,
-and discouraged. High scorers lack energy and have difficulty initiating
-activities. Low scorers tend to be free from these depressive feelings.`
+      title: 'Depresija',
+      text: `Ši skalė matuoja polinkį jaustis liūdnam, prislėgtam ir nusivylusiam. Aukštus balus surinkusiems trūksta energijos ir jiems sunku pradėti veiklas. Žemus balus surinkusiems asmenims šių depresijos jausmų paprastai nebūna.`
     },
     {
       facet: 4,
-      title: 'Self-Consciousness',
-      text: `Self-conscious individuals are sensitive
-about what others think of them. Their concern about rejection and
-ridicule cause them to feel shy and uncomfortable around others. They
-are easily embarrassed and often feel ashamed. Their fears that others
-will criticize or make fun of them are exaggerated and unrealistic, but
-their awkwardness and discomfort may make these fears a self-fulfilling
-prophecy. Low scorers, in contrast, do not suffer from the mistaken
-impression that everyone is watching and judging them. They do not feel
-nervous in social situations.`
+      title: 'Drovumas',
+      text: `Drovūs asmenys yra jautrūs tam, ką kiti apie juos galvoja. Dėl nerimo dėl atstūmimo ir pajuokos jie jaučiasi drovūs ir nejaukiai šalia kitų. Jie lengvai susigėsta ir dažnai jaučiasi sugėdinti. Jų baimės, kad kiti juos kritikuos ar iš jų pasijuoks, yra perdėtos ir nerealistiškos, tačiau
+jų nerangumas ir diskomfortas gali paversti šias baimes savaime išsipildančia pranašyste. Priešingai, žemus balus turintys asmenys neturi klaidingo
+įspūdžio, kad visi juos stebi ir teisia. Jie nesijaučia nervingi socialinėse situacijose.`
     },
     {
       facet: 5,
-      title: 'Immoderation',
-      text: `Immoderate individuals feel strong cravings and
-urges that they have have difficulty resisting. They tend to be
-oriented toward short-term pleasures and rewards rather than long-
-term consequences. Low scorers do not experience strong, irresistible
-cravings and consequently do not find themselves tempted to overindulge.`
+      title: 'Nesaikingumas',
+      text: `Nesaikingi asmenys jaučia stiprius potraukius ir instinktus, kuriems jiems sunku atsispirti. Jie linkę orientuotis į trumpalaikius malonumus ir atlygį, o ne į ilgalaikes pasekmes. Žemą balų skaičių turintys asmenys nepatiria stiprių, nenugalimų potraukių ir todėl nejaučia pagundos persistengti.`
     },
     {
       facet: 6,
-      title: 'Vulnerability',
-      text: `High scorers on Vulnerability experience
-panic, confusion, and helplessness when under pressure or stress.
-Low scorers feel more poised, confident, and clear-thinking when
-stressed.`
+      title: 'Pažeidžiamumas',
+      text: `Aukštus pažeidžiamumo balus surinkę asmenys patiria paniką, sumišimą ir bejėgiškumą, kai patiria spaudimą ar stresą. Žemus balus surinkę asmenys jaučiasi labiau nusiteikę, pasitikintys savimi ir aiškiai mąsto, kai patiria stresą.`
     }
   ]
 }
