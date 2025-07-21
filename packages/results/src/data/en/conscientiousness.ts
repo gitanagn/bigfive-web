39bc08df24d66a561650b74c2a0c6d3e4659aaed
Gitana Ginkevice <gitanagn@users.noreply.github.com>
2025-07-21 15:47:06 +0300

    Update conscientiousness.ts

--- packages/results/src/data/en/conscientiousness.ts
+++ packages/results/src/data/en/conscientiousness.ts
@@ -2,134 +2,63 @@ import { type TemplateDomain } from '../../types'
 
 const conscientiousness: TemplateDomain = {
   domain: 'C',
-  title: 'Conscientiousness',
-  shortDescription: 'Conscientiousness concerns the way in which we control, regulate, and direct our impulses.',
-  description: `Impulses are not inherently bad;
-occasionally time constraints require a snap decision, and acting on
-our first impulse can be an effective response. Also, in times of
-play rather than work, acting spontaneously and impulsively can be
-fun. Impulsive individuals can be seen by others as colorful,
-fun-to-be-with, and zany.
+  title: 'Sąmoningumas',
+  shortDescription: 'Sąmoningumas (angl. conscientiousness) apibūdina, kaip žmogus valdo, reguliuoja ir nukreipia savo impulsus.',
+  description: `Impulsai savaime nėra blogi. Kartais laiko trūkumas reikalauja greito sprendimo, o veikimas pagal pirmą impulsą gali būti efektyvus atsakas. Taip pat, žaidimo ar laisvalaikio metu spontaniškas ir impulsyvus elgesys gali būti smagus. Impulsyvūs žmonės kitiems gali pasirodyti kaip spalvingi, smagūs ir nutrūktgalviški.
 <br /><br />
-Nonetheless, acting on impulse can lead to trouble in a number of
-ways. Some impulses are antisocial. Uncontrolled antisocial acts
-not only harm other members of society, but also can result in
-retribution toward the perpetrator of such impulsive acts. Another
-problem with impulsive acts is that they often produce immediate
-rewards but undesirable, long-term consequences. Examples include
-excessive socializing that leads to being fired from one's job,
-hurling an insult that causes the breakup of an important
-relationship, or using pleasure-inducing drugs that eventually
-destroy one's health.
+Vis dėlto impulsyvus elgesys gali sukelti įvairių problemų. Kai kurie impulsai yra nesocialūs. Nekontroliuojami antisocialūs veiksmai ne tik kenkia kitiems visuomenės nariams, bet ir gali sukelti pasekmes pačiam veikėjui. Kita problema – impulsyvūs veiksmai dažnai suteikia momentinį malonumą, tačiau turi nepageidaujamų ilgalaikių pasekmių. Pavyzdžiui: besaikis plepumas gali baigtis darbo praradimu, pasakytas įžeidimas – svarbių santykių nutrūkimu, o malonumą sukeliantys narkotikai – sveikatos sužalojimu.
 <br /><br />
-Impulsive behavior, even when not seriously destructive, diminishes
-a person's effectiveness in significant ways. Acting impulsively
-disallows contemplating alternative courses of action, some of which
-would have been wiser than the impulsive choice. Impulsivity also
-sidetracks people during projects that require organized sequences
-of steps or stages. Accomplishments of an impulsive person are
-therefore small, scattered, and inconsistent.
+Impulsyvus elgesys, net jei ir nėra labai destruktyvus, gali reikšmingai sumažinti žmogaus veiksmingumą. Veikdami impulsyviai, žmonės neturi galimybės apsvarstyti alternatyvių veiksmų variantų – kai kurie jų galėtų būti žymiai protingesni nei spontaniškas pasirinkimas. Impulsyvumas taip pat trukdo susikaupti vykdant projektus, kuriems reikia organizuoto, nuoseklaus darbo. Todėl impulsyvaus žmogaus pasiekimai dažnai būna nedideli, padriki ir nenuoseklūs.
 <br /><br />
-A hallmark of intelligence, what potentially separates human beings
-from earlier life forms, is the ability to think about future
-consequences before acting on an impulse. Intelligent activity
-involves contemplation of long-range goals, organizing and planning
-routes to these goals, and persisting toward one's goals in the face
-of short-lived impulses to the contrary. The idea that intelligence
-involves impulse control is nicely captured by the term prudence, an
-alternative label for the Conscientiousness domain. Prudent means
-both wise and cautious.
+Vienas iš intelekto požymių – ir tai, kas galimai išskiria žmones iš ankstesnių gyvybės formų – yra gebėjimas prieš veikiant impulsyviai pagalvoti apie būsimas pasekmes. Intelektuali veikla apima ilgalaikių tikslų numatymą, planavimą, kaip jų pasiekti, ir atkaklumą siekiant šių tikslų, nepaisant trumpalaikių impulsų. Yra toks terminas „apdairumas“ (angl. prudence), kuris dažnai vartojamas sąmoningumui apibūdinti. Apdairus – reiškia tiek išmintingas, tiek atsargus.
 <br /><br/>
-Persons who score high on the
-Conscientiousness scale are, in fact, perceived by others as intelligent.
-The benefits of high conscientiousness are obvious. Conscientious
-individuals avoid trouble and achieve high levels of success through
-purposeful planning and persistence. They are also positively
-regarded by others as intelligent and reliable. On the negative
-side, they can be compulsive perfectionists and workaholics.
-Furthermore, extremely conscientious individuals might be regarded
-as stuffy and boring.
+Asmenys, kurių sąmoningumo savybė stipriai išreikšta, kitų dažnai yra laikomi intelektualiais. Aukšto sąmoningumo privalumai akivaizdūs: tokie žmonės vengia problemų, pasiekia aukštų rezultatų per tikslingą planavimą ir atkaklumą. Jie taip pat vertinami kaip patikimi ir protingi. Kita vertus, tokie asmenys gali tapti perfekcionistais ar darboholikais. Itin sąmoningi žmonės kartais gali būti laikomi pernelyg rimtais ar net nuobodžiais.
 <br /><br />
-Unconscientious people may be criticized for
-their unreliability, lack of ambition, and failure to stay within
-the lines, but they will experience many short-lived pleasures and
-they will never be called stuffy.`,
+Žemo sąžiningumo žmonės gali būti kritikuojami dėl nepatikimumo, ambicijų stokos ir nesugebėjimo laikytis taisyklių, tačiau jie dažnai patiria daug trumpalaikių malonumų ir niekada nebus vadinami nuobodžiais.`,
   results: [
     {
       score: 'low', // do not translate this line
-      text: `Your score on Conscientiousness is low, indicating you like to live
-for the moment and do what feels good now. Your work tends to be
-careless and disorganized.`
+      text: `Jūsų sąmoningumas yra žemas, tai rodo, kad mėgstate gyventi šia diena ir daryti tai, kas tuo metu teikia malonumą. Jūsų darbas dažnai būna neapdairus ir neorganizuotas.`
     },
     {
       score: 'neutral', // do not translate this line
-      text: `Your score on Conscientiousness is average. This means you are
-reasonably reliable, organized, and self-controlled.`
+      text: `Jūsų sąmoningumo įvertinimas yra vidutinis. Tai reiškia, kad esate pakankamai patikimas, organizuotas ir gebate save kontroliuoti.`
     },
     {
       score: 'high', // do not translate this line
-      text: `Your score on Conscientiousness is high. This means you set clear
-goals and pursue them with determination. People regard you as
-reliable and hard-working.`
+      text: `Jūsų sąmoningumo įvertinimas yra aukštas. Tai reiškia, kad keliatės aiškius tikslus ir atkakliai jų siekiate. Žmonės jus vertina kaip patikimą ir darbštų žmogų.`
     }
   ],
   facets: [
     {
       facet: 1,
-      title: 'Self-Efficacy',
-      text: `Self-Efficacy describes confidence in one's ability
-to accomplish things. High scorers believe they have the intelligence
-(common sense), drive, and self-control necessary for achieving success.
-Low scorers do not feel effective, and may have a sense that they are not
-in control of their lives.`
+      title: 'Saviveiksmingumas',
+      text: `Saviveiksmingumas apibūdina pasitikėjimą savo gebėjimu atlikti užduotis. Aukštą balą surinkę asmenys tiki, kad turi pakankamai intelekto (sveiko proto), ryžto ir savikontrolės sėkmei pasiekti. Žemą balą turintys asmenys dažnai jaučiasi neveiksmingi ir gali manyti, kad nekontroliuoja savo gyvenimo.`
     },
     {
       facet: 2,
-      title: 'Orderliness',
-      text: `Persons with high scores on orderliness are
-well-organized. They like to live according to routines and schedules. They
-keep lists and make plans. Low scorers tend to be disorganized and
-scattered.`
+      title: 'Organizuotumas',
+      text: `Asmenys, turintys aukštą įvertį, yra gerai organizuoti. Jie mėgsta gyventi pagal rutiną ir tvarkaraščius, sudarinėja sąrašus ir planuoja veiklas. Žemo įvertžio žmonės dažnai būna neorganizuoti ir išsiblaškę.`
     },
     {
       facet: 3,
-      title: 'Dutifulness',
-      text: `This scale reflects the strength of a person's sense
- of duty and obligation. Those who score high on this scale have a strong
- sense of moral obligation. Low scorers find contracts, rules, and
- regulations overly confining. They are likely to be seen as unreliable or
- even irresponsible.`
+      title: 'Pareigingumas',
+      text: `Tai asmenybės bruožas, apibūdinantis žmogaus polinkį laikytis moralinių principų, taisyklių ir įsipareigojimų. Pareigingi žmonės jaučia atsakomybę už savo veiksmus, stengiasi elgtis sąžiningai ir etiškai. Žemo įvertinimo asmenys gali būti labiau linkę ignoruoti taisykles ar priimti sprendimus pagal asmeninį patogumą.`
     },
     {
       facet: 4,
-      title: 'Achievement-Striving',
-      text: `Individuals who score high on this
-scale strive hard to achieve excellence. Their drive to be recognized as
-successful keeps them on track toward their lofty goals. They often have
-a strong sense of direction in life, but extremely high scores may
-be too single-minded and obsessed with their work. Low scorers are content
-to get by with a minimal amount of work, and might be seen by others
-as lazy.`
+      title: 'Tikslų siekimas',
+      text: `Asmenys, turintys stipriai išreikštą polinkį siekti tikslų, atkakliai siekia meistriškumo. Juos motyvuoja noras būti pripažintiems kaip sėkmingiems, todėl jie nuosekliai juda savo ambicingų tikslų link. Jie dažnai turi aiškią gyvenimo kryptį, tačiau itin aukšti įvertinimai gali rodyti pernelyg siaurą mąstymą ir darboholizmą. Žemo įvertinimo žmonės patenkinti atlikdami tik minimalų darbą, o aplinkiniams gali pasirodyti tingūs.`
     },
     {
       facet: 5,
-      title: 'Self-Discipline',
-      text: `Self-discipline-what many people call
-will-power-refers to the ability to persist at difficult or unpleasant
-tasks until they are completed. People who possess high self-discipline
-are able to overcome reluctance to begin tasks and stay on track despite
-distractions. Those with low self-discipline procrastinate and show poor
-follow-through, often failing to complete tasks-even tasks they want very
-much to complete.`
+      title: 'Savidisciplina',
+      text: `Savikontrolė – tai, ką daugelis vadina valia – reiškia gebėjimą atkakliai dirbti ties sunkiais ar nemaloniais uždaviniais iki jų pabaigos. Žmonės, turintys aukštą savidisciplinos lygį, geba įveikti nenorą pradėti darbą ir išlaikyti dėmesį, nepaisydami trikdžių. Tuo tarpu turintieji žemą savidisciplinos lygį linkę atidėlioti ir sunkiai įgyvendina užsibrėžtus tikslus – net ir tuos, kuriuos jiems iš tikrųjų labai svarbu pasiekti.`
     },
     {
       facet: 6,
-      title: 'Cautiousness',
-      text: `Cautiousness describes the disposition to
-think through possibilities before acting. High scorers on the Cautiousness
-scale take their time when making decisions. Low scorers often say or do
-first thing that comes to mind without deliberating alternatives and the
-probable consequences of those alternatives.`
+      title: 'Atsargumas',
+      text: `Atsargumas apibūdina polinkį apgalvoti galimybes prieš veikiant. Aukštus balus surenkantys žmonės šioje skalėje neskuba priimti sprendimų – jie skiria laiko apsvarstyti. Žemus balus surenkantys asmenys dažnai iškart pasako ar padaro tai, kas šauna į galvą, neapgalvodami alternatyvų ir galimų pasekmių.`
     }
   ]
 }
