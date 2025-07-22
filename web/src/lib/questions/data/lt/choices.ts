181fa9c201922835de20269d72e8c0a5dee07b5d
Gitana Ginkevice <gitanagn@users.noreply.github.com>
2025-07-22 13:13:05 +0000

    Perkeltas questions paketas į lib

---
+++ web/src/lib/questions/data/lt/choices.ts
@@ -0,0 +1,58 @@
+const choices = {
+  plus: [
+    {
+      text: 'Visiškai nesutinku',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'Nesutinku',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'Nei sutinku, nei nesutinku',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Sutinku',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'Visiškai sutinku',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Visiškai nesutinku',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'Nesutinku',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'Nei sutinku, nei nesutinku',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Sutinku',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'Visiškai sutinku',
+      score: 1,
+      color: 5
+    }
+  ]
+}
+
+export default choices
